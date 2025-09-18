f25accec32223391b7840ab2444f5eae765a8a5e
nrsltanovnrali-byte <nrsltanovnrali@gmail.com>
2025-09-18 19:43:01 +0600

    First lab

---
+++ 1/Numbers.py
@@ -0,0 +1,33 @@
+# 1
+x = 5
+x = float(x)
+
+
+# 2
+x = 5.5
+x = int(x)
+
+
+# 3
+x = 5
+x = complex(x)
+
+
+
+
+# Examples 
+
+
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+
+
+#convert from int to float:
+# a = float(x)
+
+#convert from float to int:
+# b = int(y)
+
+#convert from int to complex:
+# c = complex(x)
---
+++ 1/Syntax.py
@@ -0,0 +1,7 @@
+# 1
+print("Hello world")
+
+
+# 2
+if(5 > 2):
+    print("Yes")
