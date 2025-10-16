717e670b4cf4575a5a2a860c2aa3f42f8b7db448
nrsltanovnrali-byte <nrsltanovnrali@gmail.com>
2025-10-16 22:14:21 +0600

    Create regex_tasks.py

---
+++ lab5/regex_tasks.py
@@ -0,0 +1,50 @@
+import re
+
+text = input()
+
+# 1
+if re.fullmatch(r"ab*", text):
+    print("Match")
+else:
+    print("No match")
+
+# 2
+if re.fullmatch(r"ab{2,3}", text):
+    print("Match")
+else:
+    print("No match")
+
+# 3
+result = re.findall(r"[a-z]+_[a-z]+", text)
+print(result)
+
+# 4
+result = re.findall(r"[A-Z][a-z]+", text)
+print(result)
+
+# 5
+if re.fullmatch(r"a.*b", text):
+    print("Match")
+else:
+    print("No match")
+
+# 6
+result = re.sub(r"[ ,.]", ":", text)
+print(result)
+
+# 7
+s = ''.join(word.title() if i else word for i, word in enumerate(text.split('_')))
+print(s)
+
+# 8
+result = re.split(r'(?=[A-Z])', text)
+print(result)
+
+# 9
+result = re.sub(r"([A-Z])", r" \1", text).strip()
+print(result)
+
+# 10
+result = re.sub(r'([A-Z])', r'_\1', text).lower()
+print(result)
+
